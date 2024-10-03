bef10ea12497c4a5b9498e8ce8e308e74a8bf3e8
Landen Tomlin <145607823+tomlinl1@users.noreply.github.com>
2024-10-03 10:23:31 -0400

    Added link to sign up page

--- index.php
+++ index.php
@@ -124,6 +124,7 @@ session_start();
                             <div class="input-group">
                                 <input class="form-control" type="text" placeholder="Enter search term..." aria-label="Enter search term..." aria-describedby="button-search" />
                                 <button class="btn btn-primary" id="button-search" type="button">Go!</button>
+                                <p>New to our website?<a href="signup.php">Click here to SignUp<a><p>
                             </div>
                         </div>
                     </div>
