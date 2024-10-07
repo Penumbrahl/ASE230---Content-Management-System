15ab108661aa702c27838bb95f5cd5cc25edc21e
Landen Tomlin <145607823+tomlinl1@users.noreply.github.com>
2024-10-06 22:20:24 -0400

    Update detail.php

--- MusicPost/detail.php
+++ MusicPost/detail.php
@@ -1,2 +1,5 @@
 <?php
 session_start();
+
+// Create the main post page, this will include a comment section, but it should only be accessible to users, I (landen) can
+// work with the PHP to get this to work correctly and ensure that only users can access the blog pages.
