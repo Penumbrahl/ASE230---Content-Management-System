2616f2df6edef4bb113f7b1440186325705c16aa
Landen Tomlin <145607823+tomlinl1@users.noreply.github.com>
2024-10-08 14:53:12 -0400

    Edit Need to Sign in Page

--- MusicPost/index.php
+++ MusicPost/index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if(!isset($_SESSION['email'])){
-    echo '<p> You are not signed in. Click here to <a style="text-decoration: none" href="../signin.php" ><button class="btn btn-primary" id="button-signin" type="button">Sign In</button></a>';
+    echo '<h2>You are not signed in. Click here to <a href="../signin.php" >Sign In</a></h2>';
      die();
 }
 ?>
