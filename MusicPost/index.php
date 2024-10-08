b3c706d656c7f4ae37faa011976c0df27dc77123
Landen Tomlin <145607823+tomlinl1@users.noreply.github.com>
2024-10-08 10:40:08 -0400

    Update index.php

--- MusicPost/index.php
+++ MusicPost/index.php
@@ -115,9 +115,9 @@ session_start();
                         </ul>
                     </nav>
                 </div>
-                <!-- Side widgets-->
-                <div class="col-lg-4">
-                    <!-- Categories widget-->
+                <!-- Side widgets
+                <div class="col-lg-4">-->
+                    <!-- Categories widget
                     <div class="card mb-4">
                         <div class="card-header">Categories</div>
                         <div class="card-body">
@@ -138,13 +138,13 @@ session_start();
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <!-- Side widget-->
+                    </div>-->
+                    <!-- Side widget
                     <div class="card mb-4">
                         <div class="card-header">Side Widget</div>
                         <div class="card-body">You can put anything you want inside of these side widgets. They are easy to use, and feature the Bootstrap 5 card component!</div>
                     </div>
-                </div>
+                </div>-->
             </div>
         </div>
         <!-- Footer-->
