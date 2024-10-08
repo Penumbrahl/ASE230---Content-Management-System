e46348258dfacd9a947e9347e5e79c169e8d34f5
Landen Tomlin <145607823+tomlinl1@users.noreply.github.com>
2024-10-08 09:43:33 -0400

    Update signin.php

--- signin.php
+++ signin.php
@@ -1,7 +1,7 @@
 <?php
  session_start();
  require_once('auth.php');
- if ( return isset($_SESSION['email'])) {
+ if (return isset($_SESSION['email'])) {
      header('location: index.php');
      die();
  };
