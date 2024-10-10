f89132e599c2ca3747319c432cbb79eeb7a37db8
Landen Tomlin <145607823+tomlinl1@users.noreply.github.com>
2024-10-09 21:41:26 -0400

    copyright note

--- MusicPost/index.php
+++ MusicPost/index.php
@@ -153,7 +153,7 @@ if(!isset($_SESSION['email'])){
         </div>
         <!-- Footer-->
         <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p></div>
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; GrooveNest 2024</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
