cc665227674a1743fee549d8fb9fb2bb81b46c49
Penumbrahl <jh.harrisj04@gmail.com>
2024-10-10 09:41:11 -0400

    Update create.php

--- MusicPost/create.php
+++ MusicPost/create.php
@@ -1,2 +1,119 @@
 <?php
 session_start();
+
+/* Workflow for saving to the JSON:
+    decode original array
+    read file & array
+    append new entry onto end of array
+    overwrite file with whole new array
+    */
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>GrooveNest - Music Content Blog</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="../assets/favicon.ico" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="../css/styles.css" rel="stylesheet" />
+    </head>
+    <body>
+        <!-- Responsive navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="#!">GrooveNest</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link active" aria-current="page" href="#">Blog</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Page header with logo and tagline-->
+        <header class="py-5 bg-light border-bottom mb-4">
+            <div class="container">
+                <div class="text-center my-5">
+                    <h1 class="fw-bolder">Welcome to GrooveNest!</h1>
+                    <p class="lead mb-0">A Bootstrap 5 starter layout for your next blog homepage</p>
+                </div>
+            </div>
+        </header>
+        <!-- Page content-->
+        <div class="container">
+            <div class="row">
+                <!-- Blog entries-->
+                <div class="col-lg-8">
+                    <!-- Featured blog post-->
+                    <div class="card mb-4">
+                        <div class="card-body">
+                            <div class="small text-muted">January 1, 2023</div>
+                            <h2 class="card-title">Create Your Post</h2>
+                        </div>
+                        <div class="card-body">
+                            <form method="POST">
+                                <div class="mb-3">
+                                    <label for="title" class="form-label">Post Title:</label>
+                                    <input type="text" class="form-control" name="title" required><br />
+                                    <label for="genre" class="form-label">Genre:</label>
+                                    <input type="text" class="form-control" name="genre" required><br />
+                                    <label for="Image Upload" class="form-label">Image (URL):</label>
+                                    <input type="text" class="form-control" name="image" required><br />
+                                    <label for="content" class="form-label">Content:</label>
+                                    <input type="text" class="form-control" name="content" required><br />
+                                    <label for="title" class="form-label">Date:</label>
+                                    <input type="text" class="form-control" name="date" required><br />
+                                    <label for="title" class="form-label">Post Title:</label>
+                                    <input type="text" class="form-control" name="title" required><br />
+
+
+
+                                </div>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+                <!-- Side widgets-->
+                <div class="col-lg-4">
+                    <!-- Search widget-->
+                    <div class="card mb-4">
+                        <div class="card-header">Comments</div>
+                        <div class="card-body">
+                            <div class="input-group">
+                            <form method="POST">
+                                <div class="mb-3">
+                                    <label for="email" class="form-label">Comments:#Logic not Setup</label>
+                                    <input type="text" class="form-control" name="comment">
+                                </div>
+                                    <button class="btn btn-primary" id="button-search" type="button">Post</button>
+                                </div>
+                            </form>
+                            <br />
+                        </div>
+                    </div>
+                    <!-- Side widget-->
+                    <div class="card mb-4">
+                        <div class="card-header">Side Widget</div>
+                        <div class="card-body">You can put anything you want inside of these side widgets. They are easy to use, and feature the Bootstrap 5 card component!</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer-->
+        <footer class="py-5 bg-dark">
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2024</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
