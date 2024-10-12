1cdc0637790ac3bb02631c81c6006ddf8ecdf096
Landen Tomlin <145607823+tomlinl1@users.noreply.github.com>
2024-10-12 10:46:51 -0400

    Update users.csv.php

--- users.csv.php
+++ users.csv.php
@@ -1,9 +1,3 @@
 <?php die(); ?>
 land.toml32@gmail.com;$2y$10$7eysqcPidAak5xiIhQDxXu7231t4lRbVnplKzuhurBKohwceUqZ6a
-<<<<<<< Updated upstream
-blyad@gmail.com;$2y$10$pvML6Defkq7QlClTc9KU8..ya0fZH4igg2b9k5DHmmdqWfwI/8cDq
-boner@balls.com;$2y$10$xhqOdTrIXt7j0l2mXabPHephPUkSf4hMXlgUY9R6B2lJpLQFAhC96
-hoffmeierm1@mymail.nku.edu;$2y$10$zLlEOsDw90FIPSDK1oyjXu6ZwTPKHMJixxW.FD9NSG/ZJdNivO7Qa
-=======
 harrisj46@mymail.nku.edu;$2y$10$QNtCa5NW5/D508yCrJiZNuVIDwpKurjDMaURjPQtVunI9x7UK4Hv2
->>>>>>> Stashed changes
